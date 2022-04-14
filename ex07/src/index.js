78639740619a3513a1652e83f32d1f7068ad3f96
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-14 23:08:51 +0200

    Modified ex07/src/index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,10 +1,7 @@
 // Only change code below this line
 const numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
-const uniqueArray = new Set();
-for (var i = 0; i < numberArray.length; i++){
-    uniqueArray.add(numberArray[i]);
-}
-
-console.log([...uniqueArray].sort((a, b) => a - b));
+const uniqueArray = [...new Set(numberArray)];
+console.log(uniqueArray);
+console.log(uniqueArray.sort((a, b) => a - b));
 // Only change code above this line
 module.exports = { numberArray, uniqueArray};
---
+++ ex08/src/index.js
@@ -0,0 +1,4 @@
+// Only change code below this line
+
+// Only change code above this line
+module.exports = { unionSet, intersectionSet, differenceSet };
