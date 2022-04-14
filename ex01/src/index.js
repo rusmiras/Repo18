9d1ead56376ea3caaae9af68b10c54585db34b9b
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-14 11:05:09 +0200

    Generator multiplication function

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+let myArr = [];
+// Only change code below this line
+function* multiplication(x) {
+    for (let i = 0; i < x; i++){
+        x *= 2;
+        yield x;
+        myArr.push(x);
+    }
+}
+let num = multiplication(3);
+for (i = 0; i < 3; i++){
+    console.log(num.next().value);
+}
+// Only change code above this line
+module.exports = multiplication;
